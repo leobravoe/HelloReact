3fd652e6069006eb1ea89896fddacc4fcf3ba193
leobravoe <leonardo.bravo.estacio@gmail.com>
2026-06-25 23:11:20 -0300

    ajustando dark theme component

--- src/components/SwitchTheme/SwithTheme.jsx
+++ src/components/SwitchTheme/SwithTheme.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
+import React from "react";
 
-const SwithTheme = () => {
-    const switchClick = () => {
-        const mode = document.querySelector("html").getAttribute("data-bs-theme");
-        if(mode == "dark")
-            document.querySelector("html").setAttribute("data-bs-theme", "light");
-        else
-            document.querySelector("html").setAttribute("data-bs-theme", "dark");
-    }
+const SwitchTheme = () => {
+    const [dark, setDark] = React.useState(localStorage.getItem("theme") === "dark");
+
+    React.useEffect(() => {
+        const theme = dark ? "dark" : "light";
+        document.documentElement.setAttribute("data-bs-theme", theme);
+        localStorage.setItem("theme", theme);
+    }, [dark]);
+
     return (
         <span className="form-check-inline form-switch mx-1">
             <input
                 className="form-check-input"
-                type="checkbox" role="switch"
-                id="switchCheckDefault"
-                onChange={switchClick}
+                type="checkbox"
+                role="switch"
+                id="switchTheme"
+                checked={dark}
+                onChange={(event) => setDark(event.target.checked)}
             />
         </span>
-    )
-}
+    );
+};
 
-export default SwithTheme
+export default SwitchTheme;
